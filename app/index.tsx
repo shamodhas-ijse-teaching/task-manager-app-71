1dabec571f9c9a1ad0680fca6daa82413a42365a
shamodhas <shamodha7@gmail.com>
2025-12-17 09:09:12 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../global.css"
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+export default Index
